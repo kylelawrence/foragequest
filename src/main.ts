08331b81b8aba21dd324a74ff9db6e10aa388609
Kyle Lawrence <lawrencekyle@live.com>
2022-09-05 16:49:22 -0700

    Cleanup and demo

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,42 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import Phaser from 'phaser';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+new Phaser.Game({
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+		},
+	},
+	scene: {
+		preload: preload,
+		create: create,
+	},
+	parent: 'app',
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function preload(this: Phaser.Scene) {
+	this.load.setBaseURL('https://labs.phaser.io');
+	this.load.image('sky', 'assets/skies/space3.png');
+	this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+	this.load.image('red', 'assets/particles/red.png');
+}
+
+function create(this: Phaser.Scene) {
+	this.add.image(400, 300, 'sky');
+
+	const particles = this.add.particles('red');
+	const emitter = particles.createEmitter({
+		speed: 100,
+		scale: { start: 1, end: 0 },
+		blendMode: 'ADD',
+	});
+
+	const logo = this.physics.add.image(400, 100, 'logo');
+	logo.setVelocity(200, 200);
+	logo.setBounce(1, 1);
+	logo.setCollideWorldBounds(true);
+
+	emitter.startFollow(logo);
+}
