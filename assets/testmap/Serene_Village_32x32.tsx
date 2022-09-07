b2402dcb8ee1a35289b4aaa1c9bc6a4764c123e6
Kyle Lawrence <lawrencekyle@live.com>
2022-09-06 20:31:00 -0700

    Tileset, characters, testmap

---
+++ assets/testmap/Serene_Village_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Serene_Village_32x32" tilewidth="32" tileheight="32" tilecount="855" columns="19">
+ <image source="SERENE_VILLAGE_REVAMPED/Serene_Village_32x32.png" trans="000000" width="608" height="1440"/>
+</tileset>
