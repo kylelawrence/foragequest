35e00472e3ae2b75b2871fff1352ab9d26fccef9
Kyle Lawrence <lawrencekyle@live.com>
2022-09-19 17:19:01 -0700

    Collecting via red square

--- src/Game.ts
+++ src/Game.ts
@@ -1,7 +1,7 @@
 const output = document.getElementById('gamepad')!;
 
 const cellSize = 32;
-const initialPlayerPos = { x: 3, y: 3 };
+const initialPlayerPos = { x: 3, y: 4 };
 const initialForagableDistance = 5;
 const initialForagableCount = 20;
 
@@ -28,9 +28,10 @@ function gridPos(i: number) {
 export default class Game extends Phaser.Scene {
 	private character!: Phaser.Physics.Arcade.Group;
 	private keys!: KeyConfig;
-
+	private target!: Phaser.GameObjects.Rectangle;
 	private charDirectionRight: boolean; // Currently facing right?
-	private activeForagable: Phaser.Types.Physics.Arcade.GameObjectWithBody | null = null;
+	private foragables: Map<string, Phaser.Types.Physics.Arcade.SpriteWithStaticBody> = new Map();
+	private targetPos: { x: number; y: number } = { x: 3, y: 5 };
 
 	constructor() {
 		super('game');
@@ -40,10 +41,6 @@ export default class Game extends Phaser.Scene {
 	preload() {
 		this.keys = <KeyConfig>this.input.keyboard.addKeys('W,A,S,D,SHIFT,TAB,SPACE');
 	}
-
-	collideForagable: ArcadePhysicsCallback = (a, b) => {
-		this.activeForagable = a.name.startsWith('fora') ? a : b;
-	};
 
 	create() {
 		// Create the tileset
@@ -68,15 +65,16 @@ export default class Game extends Phaser.Scene {
 			return layer;
 		});
 
+		// Generate sign objects
 		const signsLayer = map.getObjectLayer('sign');
-		const signs = this.add.group(
-			signsLayer.objects.map((o) => {
-				return this.physics.add.staticImage(o.x! + 16, o.y! - 16, 'tiles', o.gid! - 1);
-			})
-		);
+		const signs = signsLayer.objects.map((o) => {
+			const sign = this.physics.add.staticImage(o.x! + 16, o.y! - 16, 'tiles', o.gid! - 1);
+			sign.setName('sign');
+			return sign;
+		});
 
+		// Spawn foragables
 		const takenPositions = new Set();
-		const foragables: Phaser.Types.Physics.Arcade.SpriteWithStaticBody[] = [];
 		for (var i = 0; i < initialForagableCount; i++) {
 			// Find a random position on the map that isn't on the collide layer or near the player
 			let position;
@@ -109,7 +107,7 @@ export default class Game extends Phaser.Scene {
 			foragable.body.setSize(22, 22, false);
 			foragable.body.setOffset(-2, -2);
 
-			foragables.push(foragable);
+			this.foragables.set(positionString, foragable);
 		}
 
 		// Create character from spritesheet
@@ -123,6 +121,7 @@ export default class Game extends Phaser.Scene {
 				);
 				sprite.setName(`char-${part}`);
 				sprite.setScale(2);
+				sprite.setOrigin(0.5, 1);
 
 				// Make animations for each direction
 				['right', 'left'].forEach((direction) => {
@@ -154,17 +153,19 @@ export default class Game extends Phaser.Scene {
 				sprite.body.setSize(sprite.width / 5, sprite.height / 6, false);
 				sprite.body.setOffset(13, 27);
 				sprite.setCollideWorldBounds(true);
+				sprite.setDepth(1);
 
 				return sprite;
 			})
 		);
 
-		this.character.setDepth(1);
+		// Add target rectangle
+		this.target = this.add.rectangle(gridPos(3), gridPos(5), cellSize, cellSize, undefined);
+		this.target.setStrokeStyle(2, 0xff0000, 0.6);
 
-		// Collide player with map and foragables
+		// Collide player with map, foragables, and interactables
 		this.physics.add.collider(this.character, collideLayer);
-		this.physics.add.collider(this.character, foragables, this.collideForagable);
-		this.physics.add.collider(this.character, signs, this.collideSign);
+		this.physics.add.collider(this.character, Array.from(this.foragables.values()));
 
 		// Follow camera to player and restrict to map bounds
 		this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels, true);
@@ -250,10 +251,12 @@ export default class Game extends Phaser.Scene {
 		}
 
 		// Collect foragables
-		if ((keysDown[space] || padDirections[space]) && this.activeForagable) {
-			const foragable = this.activeForagable;
-			this.activeForagable = null;
-			foragable.destroy(true);
+		if (keysDown[space] || padDirections[space]) {
+			const targetPosString = `${this.targetPos.x},${this.targetPos.y}`;
+			const foragable = this.foragables.get(targetPosString);
+			if (foragable) {
+				foragable.destroy();
+			}
 		}
 
 		// If we're to be moving
@@ -268,5 +271,27 @@ export default class Game extends Phaser.Scene {
 
 		// Set velocity
 		this.character.setVelocity(xVelocity, yVelocity);
+
+		// Move target square
+		const char: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody =
+			this.character.getFirstAlive();
+		const charX = Math.floor(char.x / 32);
+		const charY = Math.floor((char.y - 8) / 32);
+
+		if (xVelocity < 0) {
+			this.targetPos = { x: charX - 1, y: charY };
+		} else if (xVelocity > 0) {
+			this.targetPos = { x: charX + 1, y: charY };
+		}
+
+		if (yVelocity < 0) {
+			this.targetPos = { x: charX, y: charY - 1 };
+		} else if (yVelocity > 0) {
+			this.targetPos = { x: charX, y: charY + 1 };
+		}
+
+		if (xVelocity || yVelocity) {
+			this.target.setPosition(gridPos(this.targetPos.x), gridPos(this.targetPos.y));
+		}
 	}
 }
